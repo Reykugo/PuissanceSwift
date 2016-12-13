afd444059bbcff0322a359d130e40c0d3b601e1c
Developer <developer@Mac-de-Developer-2.local>
2016-12-13 15:12:22 +0100

    ajout des fichiers

---
+++ puissanceSwift/puissanceSwift/board.swift
@@ -0,0 +1,9 @@
+//
+//  board.swift
+//  PuissanceSwift
+//
+//  Created by Developer on 13/12/2016.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import Foundation
---
+++ puissanceSwift/puissanceSwift/storage.swift
@@ -0,0 +1,9 @@
+//
+//  storage.swift
+//  PuissanceSwift
+//
+//  Created by Developer on 13/12/2016.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import Foundation
