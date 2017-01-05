769ff53d12ae47fec55a8adb6e62fafa3b850b44
Developer <developer@Mac-de-Developer-2.local>
2017-01-05 12:01:47 +0100

    debut du plateau(non fonctionnel)

--- puissanceSwift/puissanceSwift/models/controllers/ViewController.swift
+++ puissanceSwift/puissanceSwift/models/controllers/ViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-     var gameManager = GameManager()
+    var gameManager = GameManager()
+    
+    var p1_image = UIImage(named:"p4_player_1.jpg")
+    var p2_image = UIImage(named:"p4_player_2.png")
+    
    
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +35,17 @@ class ViewController: UIViewController {
     
     /////////////////////////////////
 
+    @IBAction func play(_ sender: UIButton) {
+        gameManager.play_turn(column: sender.tag)
+        let player = gameManager.board.last_box_played["player"]
+        if  player == 1{
+            sender.setImage(p1_image, for: .normal)
+        }
+        else if player == 2{
+            sender.setImage(p2_image, for: .normal)
+        }
+        
+        
+    }
 
 }
-
